6e5b43348521c024fe24974cc02929aef6653769
KrishnaPrasadArisetty <155144903+KrishnaPrasadArisetty@users.noreply.github.com>
2024-11-12 15:04:01 +0000

    aa

--- BO_ATT/script/main.js
+++ BO_ATT/script/main.js
@@ -1,18 +1,19 @@
-require(["DS/DataDragAndDrop/DataDragAndDrop","DS/PlatformAPI/PlatformAPI","DS/WAFData/WAFData","DS/i3DXCompassServices/i3DXCompassServices"],
-     function(DataDragAndDrop,PlatformAPI,WAFData,BaseUrl){
+require(["DS/DataDragAndDrop/DataDragAndDrop","DS/PlatformAPI/PlatformAPI","DS/WAFData/WAFData","DS/i3DXCompassServices/i3DXCompassServices"], function(DataDragAndDrop,PlatformAPI,WAFData,BaseUrl){
 	
-    var myWidget = {
-            
-        onLoad: function () {
-        dragAndDropComp.showDroppable();
-        },
+	
+	
+    var comWidget={
         
-        updateWidget: function () {
+        onLoad: function () {
             dragAndDropComp.showDroppable();
-        }, 
+            },
+            
+        updateWidget: function () {
+                dragAndDropComp.showDroppable();
+            }, 
         
     };
-widget.addEvent('onLoad',myWidget.onLoad);
-widget.addEvent("onRefresh",myWidget.onLoad);
     
-});
+    widget.addEvent('onLoad',comWidget.onLoad);
+    widget.addEvent("onRefresh", comWidget.onLoad);
+    });
