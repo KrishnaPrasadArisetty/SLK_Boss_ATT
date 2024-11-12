455320972213e903bbdfeb668e818b9f7dbaabc3
KrishnaPrasadArisetty <155144903+KrishnaPrasadArisetty@users.noreply.github.com>
2024-11-12 15:01:43 +0000

    aaa

--- BO_ATT/script/main.js
+++ BO_ATT/script/main.js
@@ -1,4 +1,5 @@
-require(["DS/DataDragAndDrop/DataDragAndDrop","DS/PlatformAPI/PlatformAPI","DS/WAFData/WAFData","DS/i3DXCompassServices/i3DXCompassServices"], function(DataDragAndDrop,PlatformAPI,WAFData,BaseUrl){
+require(["DS/DataDragAndDrop/DataDragAndDrop","DS/PlatformAPI/PlatformAPI","DS/WAFData/WAFData","DS/i3DXCompassServices/i3DXCompassServices"],
+     function(DataDragAndDrop,PlatformAPI,WAFData,BaseUrl){
 	
     var myWidget = {
             
@@ -10,7 +11,7 @@ require(["DS/DataDragAndDrop/DataDragAndDrop","DS/PlatformAPI/PlatformAPI","DS/W
             dragAndDropComp.showDroppable();
         }, 
         
-    }
+    };
 widget.addEvent('onLoad',myWidget.onLoad);
 widget.addEvent("onRefresh",myWidget.onLoad);
     
