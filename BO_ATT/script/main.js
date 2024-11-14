e6cb7dae6a274f2e80723379a5fe98d7e0edcda2
KrishnaPrasadArisetty <155144903+KrishnaPrasadArisetty@users.noreply.github.com>
2024-11-14 05:29:04 +0000

    qa

--- BO_ATT/script/main.js
+++ BO_ATT/script/main.js
@@ -6,14 +6,10 @@ var comWidget={
 
 	onLoad: function() { 
 		var html = "<table><tr>"+
-		"<td><lable class='myLbltype'>Drop in mmmmEditor</lable></td>"+
 		"<td><input class='myInputType' type='text' size='60' /></td>"+
-		"<tr></tr>"+
-		"<td><lable class='myLbltype'>Drop status</lable></td>"+
-		"<td><input class='myStatusDrop' type='text' size='60' /></td>"+
 		"</tr></table>";
-
-		widget.body.innerHTML=html;
+		var dropbox = widget.createElement('div',{'class':'mydropclass',text :'drop objects here'});
+		widget.body.innerHTML=dropbox;
 		var theInput = widget.body.querySelector('.myInputType');
 		var theStatus = widget.body.querySelector('.myStatusDrop');
 		DataDragAndDrop.droppable(theInput,{
@@ -22,21 +18,10 @@ var comWidget={
 				theInput.value = data;
 				theStatus.value = 'drop';
 			},
-			enter : function(){
-				theStatus.value = 'enter';
-				console.log("Enter");
-			},
-			over : function(){
-				theStatus.value = 'over';
-			},
-			leave : function(){
-				theStatus.value = 'leave';
-			}
 		});
 	},
 	
 };
-
 widget.addEvent('onLoad',comWidget.onLoad);
 widget.addEvent("onRefresh", comWidget.onLoad);
 });
