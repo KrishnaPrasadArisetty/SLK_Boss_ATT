558c5088019af8c76220e63ddae1d7195f05ab9d
KrishnaPrasadArisetty <155144903+KrishnaPrasadArisetty@users.noreply.github.com>
2024-11-14 10:33:41 +0000

    aaaaaa

--- BO_ATT/script/main.js
+++ BO_ATT/script/main.js
@@ -8,8 +8,8 @@ var comWidget={
 		var html = "<table><tr>"+
 		"<td><input class='myInputType' type='text' size='60' /></td>"+
 		"</tr></table>";
-		var dropbox = widget.createElement('div',{'class':'mydropclass',text :'drop objects here'});
-		var dropimage = widget.createElement('img', {'src': 'Images/dropImage.png', 'alt': 'Dropbox Image'});
+		var dropbox = widget.createElement('div',{'class':'mydropclass',text :''});
+		var dropimage = widget.createElement('img', {'src': '../Images/dropImage.png', 'alt': 'Dropbox Image'});
 		dropbox.append(dropimage);
 		widget.body.innerHTML="";
 		dropbox.style = "border:2px #c6c5c5 dashed; margin:10px; padding: 5%; text-align: center";
